c2642846efbca1f16dca6c8d700b4abfbb89e231
Erik Iverson <erik@sigmafield.org>
2020-10-07 07:25:32 -0700

    add cache support

--- R/load_cached_dataset.R
+++ R/load_cached_dataset.R
@@ -1,16 +1,8 @@
-load_cached_dataset <- function(filename) {
-  read.csv(
-    file.path(here("data", paste0(filename, ".csv"))),
-    stringsAsFactors = FALSE) %>%
-    mutate(
-      filename = filename,
-      year = ifelse(
-        test = grepl("submitted", study_ID),
-        yes = Inf,
-        no = stringr::str_extract(study_ID, "([:digit:]{4})"))
-    ) %>%
-    mutate(
-      study_ID = as.character(study_ID),
-      same_infant = as.character(same_infant),
-      expt_condition = as.character(expt_condition))
+#' @export
+load_cached_data <- function(rdata_file) {
+  if(file.exists(rdata_file)) {
+    load(rdata_file)
+  } else {
+    cat("File ", rdata_file, " not found.")
+  }
 }
