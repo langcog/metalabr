4c985c7bfe1069676aaaf074ed6e91ef216bb108
Erik Iverson <erik@sigmafield.org>
2020-10-07 08:06:23 -0700

    load objects to .GlobalEnv

--- R/load_cached_dataset.R
+++ R/load_cached_dataset.R
@@ -1,7 +1,7 @@
 #' @export
 load_cached_data <- function(rdata_file) {
   if(file.exists(rdata_file)) {
-    load(rdata_file)
+    load(rdata_file, envir = .GlobalEnv)
   } else {
     cat("File ", rdata_file, " not found.")
   }
